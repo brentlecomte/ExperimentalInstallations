341fb131e1a53223837f587a71f8effb48df4304
Brent Le Comte <brentlecomte@hotmail.com>
2018-11-29 21:51:22 +0100

    only adding if not existent yet, but bug with id

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 {
   const osc = require("osc");
+  let currentTags = [];
 
   const udpPort = new osc.UDPPort({
     localAddress: "localhost",
@@ -10,9 +11,32 @@
     udpPort.open();
   };
 
-  udpPort.on("message", oscMsg => {
-    console.log("An OSC message just arrived!", oscMsg);
+  udpPort.on("message", Tag => {
+    if (Tag.args[0] === "set") {
+      console.log(Tag);
+
+      //add figure logic
+      checkTags(Tag.args[2]);
+    } else {
+      //Prompt to add icon
+      // console.log("no tag detected");
+    }
+    deleteTags(Tag.args[1]);
   });
+
+  const checkTags = currentTag => {
+    const checkTag = currentTag;
+    if (!currentTags.includes(checkTag)) {
+      currentTags.push(checkTag);
+      // console.log(currentTags);
+    } else {
+      // console.log("already on playfield");
+    }
+  };
+
+  const deleteTags = tagToDelete => {
+    // console.log("delete");
+  };
 
   init();
 }
